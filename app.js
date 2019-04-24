668607de99f85e0207f9037939313821a4a3df6b
Sean <nortonseanm@gmail.com>
2019-04-24 15:12:26 -0400

    write html as response

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var port = process.env.PORT || 3000;
 var app = express();
 
 app.get('/', function (req, res) {
- res.send(JSON.stringify({ Hello: 'World'}));
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('<h1>Hello World</h1>');
+    response.end();
 });
 
 app.listen(port, function () {
